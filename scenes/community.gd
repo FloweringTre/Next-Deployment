c01c735226c43589efff0f2df7c985658d5cedcf
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2026-02-24 21:54:42 -0700

    swap button set for data pad

--- scenes/community.gd
+++ scenes/community.gd
@@ -77,9 +77,7 @@ func _on_global_interaction(reciever, sender, message):
 			
 			if sender == "popup_box":
 				$CanvasLayer/HBoxContainer/DayButton.disabled = dialog_open
-				$CanvasLayer/HBoxContainer/ListButton.disabled = dialog_open
-				$CanvasLayer/HBoxContainer/CostsButton.disabled = dialog_open
-				$CanvasLayer/HBoxContainer/WantsButton.disabled = dialog_open
+				$CanvasLayer/HBoxContainer/DatapadButton.disabled = dialog_open
 				$CanvasLayer.visible = true
 
 
@@ -101,34 +99,6 @@ func _on_button_pressed() -> void:
 	#print("newdaybuttonpressed")
 
 
-func _on_list_button_pressed() -> void:
-	var temp = ""
-	var list = ""
-	for item in Globals.Checklist:
-		#print(item)
-		temp = " " + str(Globals.Checklist[item]["has"]) + "/" + str(Globals.Checklist[item]["need"]) + " - " + str(Globals.Checklist[item]["name"] + "\n")
-		list += temp
-		temp = ""
-	
-	%PopupBox.open("GAR DEPLOYMENT CHECKLIST", list)
-
-func _on_costs_button_pressed() -> void:
-	var list = "These values will be added/removed to your resources at the start at each day. \n\n"
-	list += "  Stone: " + str(BuySheet.DailyCosts["stone"]) + "\n"
-	list += "  Wood: " + str(BuySheet.DailyCosts["wood"]) + "\n"
-	list += "  Max Labor: " + str(BuySheet.DailyCosts["labor"]) + "\n"
-	list += "  Water: " + str(BuySheet.DailyCosts["water"]) + "\n"
-	list += "  Food: " + str(BuySheet.DailyCosts["food"]) + "\n"
-	
-	%PopupBox.open("Daily Resource Costs", list)
-
-func _on_wants_button_pressed() -> void:
-	var list = "This is what the community wants. \n\n"
-	var temp = ""
-	for item in Globals.Checklist:
-		#print(item)
-		temp = " " + str(Globals.Checklist[item]["c_want"][1]) + "/" + str(Globals.Checklist[item]["c_want"][2]) + " - " + str(Globals.Checklist[item]["name"] + "\n")
-		list += temp
-		temp = ""
-	
-	%PopupBox.open("Community Wants", list)
+func _on_datapad_button_pressed() -> void:
+	%DataPad.visible = true
+	%DataPad.set_up()
