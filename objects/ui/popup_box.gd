48dd7c499f29f9e55a4b517e176b77f007e63808
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2026-02-25 16:39:52 -0700

    rework popup to accept multiple options

--- objects/ui/popup_box.gd
+++ objects/ui/popup_box.gd
@@ -1,17 +1,32 @@
 extends Control
 
+signal answer
+
 func _ready() -> void:
 	$".".visible = false
 
 
-func open(header: String, message: String) -> void:
+func open_popup(yes_no_type: bool, header: String, message: String) -> void:
+	%moveButton.visible = !yes_no_type
+	%y_nBox.visible = yes_no_type
 	%title.text = header
 	%text.text = message
 	$".".visible = true
-	Globals.interact_with("GLOBAL_DIALOG", "popup_box", "start")
+	print(header)
 
-func _on_button_pressed() -> void:
+func close() -> void:
 	%title.text = ""
 	%text.text = ""
 	$".".visible = false
-	Globals.interact_with("GLOBAL_DIALOG", "popup_box", "end")
+
+func _move_on_button_pressed() -> void:
+	answer.emit("continue")
+	close()
+
+func _on_no_button_pressed() -> void:
+	answer.emit("no")
+	close()
+
+func _on_yes_button_pressed() -> void:
+	answer.emit("yes")
+	close()
