9138e7ae234e54353699447e32b07b235ee36d51
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2026-03-02 22:00:34 -0700

    update the civilian object to have new assets

--- objects/civilian.gd
+++ objects/civilian.gd
@@ -4,8 +4,15 @@ var mouse_on : bool = false
 var mouse_pressed : bool = false
 
 @export var char_name : String
-@export var char_color : Color = Color(1.0, 1.0, 1.0, 0.0)
-@export var start_frame : int = -1
+@export var is_trooper : bool
+
+var character_values : Dictionary = {
+	"hair" = [0, Color(1, 1, 1, 1)],
+	"top" = [0, Color(1, 1, 1, 1)],
+	"pants" = [0, Color(1, 1, 1, 1)],
+	"boots" = [0, Color(0.062, 0.026, 0.006, 1.0)],
+	"skin" = [0, Color(1, 1, 1, 1)],
+}
 
 var old_loc : Vector2
 var new_loc : Vector2
@@ -25,13 +32,11 @@ func _input(event: InputEvent) -> void:
 			mouse_pressed = event.pressed
 			
 			if mouse_pressed:
-				$Sprite2D.frame = start_frame+1
 				$AnimationPlayer.play("hang")
 				Globals.interact_with("GLOBAL_ALERT", char_name, "carrying")
 			else:
-				$Sprite2D.frame = start_frame
 				$AnimationPlayer.play("RESET")
-				$".".position += Vector2(0, 150)*$".".scale
+				$".".position += Vector2(0, 350)*$".".scale
 				Globals.interact_with("GLOBAL_ALERT", char_name, "carrying")
 	
 	elif event is InputEventMouseMotion and mouse_pressed:
@@ -41,16 +46,45 @@ func character_setup() -> void:
 	if char_name == "":
 		char_name = Globals.random_names[randi_range(0, len(Globals.random_names)-1)]
 	
-	if char_color == Color(1.0, 1.0, 1.0, 0.0):
-		char_color = Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1), 1)
+	%boots.visible = !is_trooper
+	%pants.visible = !is_trooper
+	%tops.visible = !is_trooper
+	%armor.visible = is_trooper
 	
-	if start_frame == -1:
-		start_frame = randi_range(0, 4)*2
+	if is_trooper:
+		character_values.set("skin", [0, Globals.human_tones[1]])
+		var temp_hair = randi_range(0, 3)
+		match temp_hair:
+			1:
+				character_values.set("hair", [1, Globals.hair_tones[randi_range(0, len(Globals.hair_tones)-1)]])
+			2:
+				character_values.set("hair", [2, Globals.hair_tones[randi_range(0, len(Globals.hair_tones)-1)]])
+			_: # 3 or 0
+				character_values.set("hair", [0, Color(1,1,1,1)])
 	
-	$Sprite2D.frame = start_frame
+	else:
+		if randi_range(0, 2) == 1: #human
+			character_values.set("skin", [0, Globals.human_tones[randi_range(0, len(Globals.human_tones)-1)]])
+			character_values.set("hair", [randi_range(1, 4), Globals.hair_tones[randi_range(0, len(Globals.hair_tones)-1)]])
+			character_values.set("top", [randi_range(0, 2), Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1), 1)])
+			character_values.set("pants", [randi_range(0, 2), Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1), 1)])
+		else: #twilek
+			character_values.set("skin", [0, Globals.twi_tones[randi_range(0, len(Globals.twi_tones)-1)]])
+			character_values.set("hair", [randi_range(5, 6), character_values["skin"][1]])
+			character_values.set("top", [randi_range(0, 2), Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1), 1)])
+			character_values.set("pants", [randi_range(0, 2), Color(randf_range(0, 1), randf_range(0, 1), randf_range(0, 1), 1)])
+	
 	$nameLabel.text = char_name
-	$nameLabel.add_theme_color_override("font_color", char_color)
-	$Sprite2D.self_modulate = char_color
+	
+	%tops.frame = character_values["top"][0]
+	%pants.frame = character_values["pants"][0]
+	%hair.frame = character_values["hair"][0]
+	%tops.self_modulate = character_values["top"][1]
+	%pants.self_modulate = character_values["pants"][1]
+	%hair.self_modulate = character_values["hair"][1]
+	%base.self_modulate = character_values["skin"][1]
+	%boots.self_modulate = character_values["boots"][1]
+	$AnimationPlayer.play("RESET")
 
 func _on_area_2d_mouse_entered() -> void:
 	$nameLabel.visible = true
@@ -59,3 +93,32 @@ func _on_area_2d_mouse_entered() -> void:
 func _on_area_2d_mouse_exited() -> void:
 	$nameLabel.visible = false
 	mouse_on = false
+
+func walk() -> void:
+	%base.frame = 1
+	%armor.frame = 1
+	%boots.frame = 1
+	%pants.frame = character_values["pants"][0] + 3
+	%tops.frame = character_values["top"][0] + 3
+	%hair.frame = character_values["hair"][0] + 7
+func stand() -> void:
+	%base.frame = 0
+	%armor.frame = 0
+	%boots.frame = 0
+	%pants.frame = character_values["pants"][0]
+	%tops.frame = character_values["top"][0]
+	%hair.frame = character_values["hair"][0]
+func work() -> void:
+	%base.frame = 2
+	%armor.frame = 2
+	%boots.frame = 2
+	%pants.frame = character_values["pants"][0] + 6
+	%tops.frame = character_values["top"][0] + 6
+	%hair.frame = character_values["hair"][0] + 14
+func hang() -> void:
+	%base.frame = 3
+	%armor.frame = 3
+	%boots.frame = 3
+	%pants.frame = character_values["pants"][0] + 9
+	%tops.frame = character_values["top"][0] + 9
+	%hair.frame = character_values["hair"][0] + 21
